7070ae9c154a58829d11b583310b7bbbdf23841b
James Yeates <jimibue@gmail.com>
2021-09-29 11:26:19 -0600

    dropdown working

--- client/src/pages/Cities.js
+++ client/src/pages/Cities.js
@@ -1,9 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Dropdown } from "semantic-ui-react";
 
 const Cities = () => {
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    getCities();
+  }, []);
+
+  const handleChange = (e, { value }) => {
+    console.log(value);
+  };
+  const getCities = async () => {
+    try {
+      let res = await axios.get("/api/cities");
+      // [{city:'qwe'}] => [{key:'qwe', text:'qwe', value:'qwe'}]
+      let normalizedData = res.data.map((d) => {
+        return { key: d.city, text: d.city, value: d.city };
+      });
+      setCities(normalizedData);
+      // do we need to normalize? yes
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <h1>cities</h1>
+      <Dropdown
+        onChange={handleChange}
+        placeholder="Select City"
+        fluid
+        selection
+        options={cities}
+      />
     </div>
   );
 };
