3e365b74f58f513ca8aea30099b377e601752e5a
James Yeates <jimibue@gmail.com>
2021-09-30 10:33:06 -0600

    find home be done

--- app/controllers/api/agents_controller.rb
+++ app/controllers/api/agents_controller.rb
@@ -3,4 +3,9 @@ class Api::AgentsController < ApplicationController
   def index
    render json: Agent.unsold_homes
   end
+
+  # bonus try as custom SQL
+  def show
+    render json: Agent.find(params[:id]).buyers
+  end
 end
--- app/controllers/api/buyers_controller.rb
+++ app/controllers/api/buyers_controller.rb
@@ -1,2 +1,12 @@
 class Api::BuyersController < ApplicationController
+
+  def show
+  
+    buyer = Buyer.find(params[:id])
+    # my_homes is a class method
+    render json: Buyer.my_homes(buyer.id, buyer.cities)
+    
+    # as a instance method, not passing anything
+    # render json: buyer.my_homes
+  end
 end
