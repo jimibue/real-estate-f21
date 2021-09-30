39b2efec429d1c023e928e30c204d803f9703f3c
James Yeates <jimibue@gmail.com>
2021-09-30 11:17:34 -0600

    finde home ui

--- client/src/pages/FIndHomes.js
+++ client/src/pages/FIndHomes.js
@@ -1,9 +1,79 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Dropdown } from "semantic-ui-react";
 
 const FindHomes = () => {
+  const [agents, setAgents] = useState([]);
+  const [buyers, setBuyers] = useState(null);
+  const [properties, setProperties] = useState(null);
+
+  useEffect(() => {
+    getAgents();
+  }, []);
+  const normalizeDropdownOptions = (data) => {
+    return data.map((a) => {
+      return {
+        key: a.id,
+        value: a.id,
+        text: `${a.first_name} ${a.last_name}`,
+      };
+    });
+  };
+  const getAgents = async () => {
+    try {
+      let res = await axios.get("/api/agents");
+      // {id: 13, first_name: 'Philomena', last_name: 'Abshire', sold: false, unsold_homes: 5} =>
+      // {key:id, value:id, text: first_name+last_name}
+      let normalizedAgents = normalizeDropdownOptions(res.data);
+      setAgents(normalizedAgents);
+
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAgentChange = async (e, { value }) => {
+    console.log(value);
+    try {
+      let res = await axios.get(`/api/agents/${value}`);
+      let normalizedBuyers = normalizeDropdownOptions(res.data);
+      setBuyers(normalizedBuyers);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleBuyerChange = async (e, { value }) => {
+    console.log(value);
+    try {
+      let res = await axios.get(`/api/buyers/${value}`);
+      setProperties(res.data);
+    } catch (err) {}
+  };
   return (
     <div>
       <h1>Homes</h1>
+      <Dropdown
+        placeholder="Agent"
+        search
+        selection
+        label="Select Agent"
+        options={agents}
+        onChange={handleAgentChange}
+      />
+      <br />
+      {buyers && (
+        <Dropdown
+          placeholder="Buyers"
+          search
+          selection
+          label="Select Buyer"
+          options={buyers}
+          onChange={handleBuyerChange}
+        />
+      )}
+      <br />
+      {properties && <code>{JSON.stringify(properties)}</code>}
     </div>
   );
 };
